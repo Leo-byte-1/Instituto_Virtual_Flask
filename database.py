0c8f7c3187ce01a12be9c25d62cede4a96af7de2
Leo <cardozoleonardo856@gmail.com>
2025-10-01 17:49:04 -0300

    version 1.4, se agregaron los iconos en index.html y se mejoro el estilo de lista_alumnos

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def conexion():
         "host": '127.0.0.1',
         "port": "3306",
         "user": "root",
-        "password": "",
+        "password": "82n9Y25zx",
         "database": "datosalumnos"
     }
     return mysql.connector.connect(**config)
